edd8e655885bf24926861ef7c2aaf57898ce34c1
ebubekeyz <ebubeofforjoe@gmail.com>
2024-05-13 02:39:49 +0100

    1

---
+++ models/User.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { log, timeStamp } = require('console');
+const validator = require('validator');
+const moment = require('moment');
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, 'Please enter email'],
+      validate: {
+        validator: validator.isEmail,
+        message: 'Please provide a valid email',
+      },
+      unique: true,
+    },
+
+    name: {
+      type: String,
+      required: [true, 'Please provide a name'],
+      minlength: [3, 'Name is too short'],
+    },
+    password: {
+      type: String,
+      required: [true, 'Please provide a password'],
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'owner', 'user'],
+      default: 'user',
+    },
+    ref: {
+      type: String,
+      required: [true, 'Please provide referral a referral'],
+    },
+    balance: {
+      type: Number,
+      default: 0,
+    },
+    date: {
+      type: String,
+      default: moment().format('YYYY-DD-MM'),
+    },
+  },
+  { timestamps: true }
+);
+// test
+UserSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign(
+    {
+      userId: this._id,
+      name: this.name,
+      email: this.email,
+      role: this.role,
+      ref: this.ref,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  );
+};
+
+UserSchema.methods.comparePassword = async function (canditatePassword) {
+  const isMatch = await bcrypt.compare(canditatePassword, this.password);
+  return isMatch;
+};
+
+module.exports = mongoose.model('User', UserSchema);
